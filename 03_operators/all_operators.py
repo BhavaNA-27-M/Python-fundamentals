7e2237a17dc3839b986730cbd39c840b940fa161
Bhavana <mb8807458@gmail.com>
2026-05-16 11:02:19 +0530

    Update and rename arithmetic.py to all_operators.py

---
+++ 03_operators/all_operators.py
@@ -0,0 +1,39 @@
+# Arithmetic operators
+a = 25
+b = 10
+
+print(a + b)   # addition
+print(a - b)   # subtraction
+print(a * b)   # multiplication
+print(a / b)   # division
+print(a % b)   # modulus
+print(a ** b)  # exponent
+print(a // b)  # floor division
+
+# Comparison operators
+print(a > b)   # greater than
+print(a < b)   # less than
+print(a == b)  # equal to
+print(a != b)  # not equal to
+print(a >= b)  # greater or equal
+print(a <= b)  # less or equal
+
+# Logical operators
+print(a > 10 and b > 5)
+print(a > 100 or b > 5)
+print(not a > 100)
+
+# Assignment operators
+x = 10
+x += 5
+print(x)
+x -= 3
+print(x)
+x *= 2
+print(x)
+
+# Bitwise operators
+print(a & b)   # AND
+print(a | b)   # OR
+print(a ^ b)   # XOR
+print(~a)      # NOT
--- 03_operators/arithmetic.py
+++
@@ -1,9 +0,0 @@
-# Arithmetic operators
-
-a = 25
-b = 10
-
-print(a + b)
-print(a - b)
-print(a * b)
-print(a / b)
