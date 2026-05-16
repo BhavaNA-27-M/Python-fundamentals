28f9dba9ff34501456180ad536a2e64326ac8994
Bhavana <mb8807458@gmail.com>
2026-05-16 11:18:30 +0530

    Add even/odd checker and simple login system

---
+++ 04_conditions/even_odd.py
@@ -0,0 +1,19 @@
+# Even or odd checker
+
+num = int(input("Enter a number: "))
+
+if num % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
+  
+
+# Simple login system
+
+username = input("Enter username: ")
+password = input("Enter password: ")
+
+if username == "admin" and password == "1234":
+    print("Login Successful")
+else:
+    print("Invalid Credentials")
